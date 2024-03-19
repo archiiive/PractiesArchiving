2b47ee597d9b6a7f961b826175b1f7c8e235bc39
archiiive <githubarchive10@gmail.com>
2024-03-19 23:40:39 +0900

    :monocle_face: 1

---
+++ src/P1/P_Exception.java
@@ -0,0 +1,22 @@
+package P1;
+
+public class P_Exception {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        try{
+            System.out.println(2);
+            System.out.println(3/0);
+            System.out.println(4);
+        }catch (ArithmeticException e){
+            System.out.println(5);
+        }
+            System.out.println(6);
+
+
+
+
+
+
+    }
+}
